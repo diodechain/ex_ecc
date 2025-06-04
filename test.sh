a9803385e02e5ee7df02a373f7308d6fcefa0806
Dominic Letz <dominic@diode.io>
2025-06-04 14:53:58 +0200

    wip

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-mix test test/ex_ecc/bn128_and_bls12_381_test.exs --max-failures=1 2>&1 | head -n 50
+echo "mix test test/ex_ecc/bls/ciphersuites/g2_basic_test.exs --max-failures=1 2>&1 | head -n 50"
+mix test test/ex_ecc/bls/ciphersuites/g2_basic_test.exs --max-failures=1 2>&1 | head -n 50
