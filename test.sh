95c2511fb212eaf5122c0c9ceb9fb0aa3d4924fc
Dominic Letz <dominic@diode.io>
2025-06-05 02:31:08 +0200

    wip

--- test.sh
+++ test.sh
@@ -4,6 +4,8 @@ export MIX_ENV=test
 echo "mix compile"
 mix compile --warnings-as-errors
 #TEST=test/ex_ecc/bls/ciphersuites/g2_basic_test.exs
-TEST=test/bls/g2_primitives_test.exs:19
+#TEST=test/bls/g2_primitives_test.exs:19
+TEST=test/bls/g2_core_test.exs
+#TEST=test/bls/point_compression_test.exs
 echo "mix test $TEST --seed=0 --max-failures=1 2>&1"
 mix test $TEST --seed=0 --max-failures=1 2>&1
