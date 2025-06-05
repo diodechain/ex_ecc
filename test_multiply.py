cd1740c33f61872e523513cac0223ce98f9b3bcb
Dominic Letz <dominic@diode.io>
2025-06-05 14:59:05 +0200

    wip

--- test_multiply.py
+++ test_multiply.py
@@ -1,15 +1,23 @@
+from py_ecc.bls.point_compression import (
+    compress_G1,
+    compress_G2,
+    decompress_G1,
+    decompress_G2,
+)
 from py_ecc.optimized_bls12_381 import (
-    G1,
+    G2,
     multiply,
 )
 
-def test_multiply():
-    # Test case: Verify aggregated signatures
-    pt = multiply(G1, 3)
+def test_compress():
+    pt = G2
     print(pt)
-    print("Multiply tests passed!")
+    z1, z2 = compress_G2(pt)
+    print(z1)
+    print(z2)
+    print("Compress tests passed!")
 
 if __name__ == "__main__":
-    print("Running multiply tests...")
-    test_multiply()
+    print("Running compress tests...")
+    test_compress()
     print("All tests passed!") 
