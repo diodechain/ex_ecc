dc5462529a302d10b39bc55feb29a6ee35b0b686
Dominic Letz <dominic@diode.io>
2025-06-07 01:13:22 +0200

    added ci

--- mix.exs
+++ mix.exs
@@ -2,14 +2,31 @@ defmodule ExEcc.MixProject do
   use Mix.Project
 
   def project do
-    :erlang.system_flag(:backtrace_depth, 30)
-
     [
       app: :ex_ecc,
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Hex
+      description: "ExEcc is a library for elliptic curve cryptography.",
+      package: [
+        licenses: ["Apache-2.0"],
+        maintainers: ["Dominic Letz"],
+        links: %{"GitHub" => "https://github.com/diodechain/ex_ecc"}
+      ],
+      # Docs
+      name: "ExEcc",
+      source_url: "https://github.com/diodechain/ex_ecc",
+      docs: [
+        # The main page in the docs
+        main: "ExEcc",
+        extras: ["README.md"]
+      ],
+      aliases: [
+        lint: ["format --check-formatted", "credo --strict", "dialyzer"]
+      ]
     ]
   end
 
@@ -23,6 +40,8 @@ defmodule ExEcc.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.3", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.30", only: :dev, runtime: false},
       {:while, "~> 0.2"}
     ]
